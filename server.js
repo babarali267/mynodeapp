5df253ea3f3256892a0aea4a1304ef0391be87ed
Babarali34 <maharbabar267@gmail.com>
2023-09-05 14:13:07 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine' , 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+
+const tasks = []
+
+app.get('/',(req,res)=>{
+     res.render('index')
+})
+
+
+app.get('/tasks',(req,res)=>{
+     res.render('tasks',{tasks : tasks})
+})
+
+app.post('/task',(req,res)=>{
+     const newTask = req.body.task
+     tasks.push (newTask)
+       res.redirect('/')
+   
+})
+
+
+app.listen(4000,()=>{
+    console.log("Server is runing");
+})
